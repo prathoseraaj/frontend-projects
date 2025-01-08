273fc986733430007dff6b2891d2e12c00de1332
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-09 00:36:09 +0530

    feat: Ubdate

--- RandomUsers/script.js
+++ RandomUsers/script.js
@@ -7,4 +7,12 @@ async function randomtasks() {
     });
     const data = await response.json();
     console.log(data);
+
+    document.getElementById('name').innerHTML = data.results[0].name.title + `. ` + data.results[0].name.first + ` ` + data.results[0].name.last   ;
+    document.getElementById('Email').innerHTML = data.results[0].email;
+    document.getElementById('Gender').innerHTML =data.results[0].gender;
+    document.getElementById('number').innerHTML =data.results[0].phone;
+    document.getElementById('state').innerHTML = data.results[0].location.state;
+
+
 }
