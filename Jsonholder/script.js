44f76ecc4e170bfeac7276ce63dfe3d97c8270bc
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-09 19:08:11 +0530

    feat: added jsonholder

---
+++ Jsonholder/script.js
@@ -0,0 +1,8 @@
+async function jsonholder() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const data = await response.json();
+    console.log(data);
+    
+}
+
+console.log(jsonholder())
