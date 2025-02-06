080f784c63a4478671156fff0916fde91e153eea
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-06 23:05:48 +0530

    feat: done TS

---
+++ CatCEO/script.ts
@@ -0,0 +1,7 @@
+async function fetchdata() {
+    const response = await fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1");
+    const data = await response.json();
+    console.log(data);
+}
+
+fetchdata();
